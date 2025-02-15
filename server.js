e5d6f6c725b780d55be76d47519ae783353d8e3c
“richardcrn3rd” <richardcrn3rd@gmail.com>
2025-02-15 12:32:38 -0500

    First Commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 // ROOT INDEX 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('Hello World Again')
 })
 
 // BOOKS 
